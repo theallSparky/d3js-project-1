4de616a476cf4c4d426954ee9e2aaa0d3b9fd880
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-10 11:29:16 -0600

    Cleaned up with method chaining

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
 const canvas = d3.select(".canva");
-const svg = canvas.append("svg");
-svg.attr("width", "500").attr("height", "250");
+const svg = canvas.append("svg").attr("width", "500").attr("height", "250");
